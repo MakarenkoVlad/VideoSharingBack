c88ba020b602c29e806d7d0e68b0e9d0398c31ef
MakarenkoVlad <vik1vik007@gmail.com>
2022-01-10 14:09:31 +0200

    Initial commit

---
+++ src/main/kotlin/App.kt
@@ -0,0 +1,15 @@
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+
+fun main() {
+    embeddedServer(Netty, port = 8000, host = "0.0.0.0") {
+        routing {
+            get {
+               call.respond("Hello wrld!")
+            }
+        }
+    }.start(wait = true)
+}
